f42c67ef67236a4a78acac4053f49e4364dd2edf
espinozaerica74 <espinozaerica74@gmail.com>
2019-10-24 01:24:44 +0000

    add an alert

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 $( "document" ).ready(function() {
 $("button").click(function(){
 let text = $("input").val();
-let words = text.split("")
+alert(text);
+let words = text.split(" ");
+let beg1 = words[0].charAt(0);
+let beg2 = words[1].charAt(1);
 
+let end1 = words[0].slice(1, words[0].lenght);
+let end2 = words[1].slice(1, words[1].lenght);
+$("#result").append(beg2 = end1 =''+ beg1 + end2);
 });
 });
+$()
